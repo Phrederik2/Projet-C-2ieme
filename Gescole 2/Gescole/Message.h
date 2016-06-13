d24300a0f7b183a19626cfe0498f12e1c55891e7
Frederic Evrard (Mac) <phrederik2@gmail.com>
2016-06-13 11:28:09 +0200

    Message

--- Gescole 2/Gescole/Message.h
+++ Gescole 2/Gescole/Message.h
@@ -1,14 +1,6 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-//#define LANG_FR
-
-#ifndef LANG
-#define LANG_FR
-#endif
-
-#ifdef LANG_FR 
-#define LANG
 // Ponctuation
 #define Pct_Point ". "
 #define Pct_DeuxPoint ": "
@@ -16,23 +8,33 @@
 #define Pct_Tabulation "\t"
 #define Pct_Tiret " - "
 
+#define LANG_EN
+
+#ifndef LANG
+#define LANG_FR
+#endif
+
+#ifdef LANG_FR 
+#define LANG
+
+
 //Menu
 #define Menu_eDossier "Dossier"
 #define Menu_eClient "Client"
 #define Menu_eLivraison "Livraison"
 #define Menu_eCommande "Commande"
 #define Menu_eRDV "Rendez-Vous"
-#define Menu_eSelect "Selectionner"
+#define Menu_eSelect "Selectionne"
 #define Menu_eCreate "Cree"
 #define Menu_eRead "Afficher l'enregistrement courant"
-#define Menu_eUpdate "Modifier"
+#define Menu_eUpdate "Modifie"
 #define Menu_eDelete "Effacer l'enregistrement courants"
 #define Menu_eFirst "Premier"
 #define Menu_eNext "Suivant"
 #define Menu_ePrevious "Precedent"
 #define Menu_eLast "Dernier"
 #define Menu_eList "Lister les enregistrement"
-#define Menu_eQuit "Quitter"
+#define Menu_eQuit "Quitte"
 #define Menu_Choice "Votre choix"
 
 //Titre
@@ -48,8 +50,6 @@
 #define Dossier_RDV "Rendez-vous"
 #define Dossier_Livraison "Livraison"
 #define Dossier_Commande "Commande"
-#define Dossier_ ""
-#define Dossier_ ""
 // Client
 #define Client_ID "ID"
 #define Client_Nom "Nom"
@@ -87,6 +87,65 @@
 #ifdef LANG_EN
 #define LANG
 
+//Menu
+#define Menu_eDossier "Folder"
+#define Menu_eClient "Customer"
+#define Menu_eLivraison "Delivery"
+#define Menu_eCommande "Order"
+#define Menu_eRDV "Appointment"
+#define Menu_eSelect "Selected"
+#define Menu_eCreate "Creates"
+#define Menu_eRead "View the current record"
+#define Menu_eUpdate "Amended"
+#define Menu_eDelete "Delete the current record"
+#define Menu_eFirst "First"
+#define Menu_eNext "Next"
+#define Menu_ePrevious "Previous"
+#define Menu_eLast "Last"
+#define Menu_eList "List registration"
+#define Menu_eQuit "Quits"
+#define Menu_Choice "Your choice"
+
+//Titre
+#define Titre_Lancer "Main menu"
+#define Titre_Dossier "Folder"
+#define Titre_Livraison "Delivery"
+#define Titre_Commande "Order"
+#define Titre_Client "Customer"
+#define Titre_RDV "Appiontment"
+// Dossier
+#define Dossier_ID "ID"
+#define Dossier_Client "Customer"
+#define Dossier_RDV "Appointment"
+#define Dossier_Livraison "Delivery"
+#define Dossier_Commande "Order"
+// Client
+#define Client_ID "ID"
+#define Client_Nom "Last name"
+#define Client_Prenom "First name"
+#define Client_Societe "Society"
+#define Client_Localite "Locality"
+#define Client_Rue "Street"
+#define Client_Numero "Number"
+#define Client_Boite "box"
+#define Client_CodePostal "Postal code"
+// Livraison
+#define Livraison_ID "ID"
+#define Livraison_Statut "Statut"
+// Commande
+#define Commande_ID "ID"
+#define Commande_Statut "Statut"
+// Rendez-vous
+#define RDV_ID "ID"
+#define RDV_DateDebut "Start date"
+#define RDV_DateFin "End date"
+#define RDV_Remarque "note"
+#define RDV_Debut "Start"
+#define RDV_Fin "End"
+// Date
+#define Date_Jour "Day"
+#define Date_Mois "Month"
+#define Date_Annee "Year"
 
 #endif // LANGUE == EN
 
